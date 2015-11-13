a99e98c72246c13eb87a602cdb1a6dc1b146e9a8
James Croft <jamz_c@hotmail.co.uk>
2015-11-13 23:18:04 +0000

    Added URL validation rule for validating text box.

---
+++ Croft.Core/Croft.Core.UWP/Validation/Rules/UrlValidationRule.cs
@@ -0,0 +1,29 @@
+﻿namespace Croft.Core.Validation.Rules
+{
+    using System.Text.RegularExpressions;
+
+    public class UrlValidationRule : ValidationRule
+    {
+        public override string ErrorMessage => "Value is not a valid URL.";
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            var val = value.ToString();
+            if (string.IsNullOrWhiteSpace(val))
+            {
+                return true;
+            }
+
+            const string UrlPattern =
+                @"(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?";
+
+            var reg = new Regex(UrlPattern, RegexOptions.IgnoreCase);
+            return reg.IsMatch(val);
+        }
+    }
+}
