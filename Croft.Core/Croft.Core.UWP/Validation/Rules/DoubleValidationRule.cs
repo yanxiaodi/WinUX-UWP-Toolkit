0d872fc70bd7d4f9fc752ed56aa7810b3793af7a
James Croft <jamz_c@hotmail.co.uk>
2015-11-13 23:21:14 +0000

    Added a couple of numeric validation rules for the validating text box.

---
+++ Croft.Core/Croft.Core.UWP/Validation/Rules/DoubleValidationRule.cs
@@ -0,0 +1,18 @@
+﻿namespace Croft.Core.Validation.Rules
+{
+    public class DoubleValidationRule : ValidationRule
+    {
+        public override string ErrorMessage => "Value is not a valid number.";
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            double val;
+            return double.TryParse(value.ToString(), out val);
+        }
+    }
+}
---
+++ Croft.Core/Croft.Core.UWP/Validation/Rules/IntValidationRule.cs
@@ -0,0 +1,18 @@
+﻿namespace Croft.Core.Validation.Rules
+{
+    public class IntValidationRule : ValidationRule
+    {
+        public override string ErrorMessage => "Value is not a valid number.";
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            int val;
+            return int.TryParse(value.ToString(), out val);
+        }
+    }
+}
