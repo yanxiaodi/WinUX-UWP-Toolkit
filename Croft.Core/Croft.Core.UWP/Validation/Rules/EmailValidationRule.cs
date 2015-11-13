f9db7ec922ec4040d7fe864eb8b150702a3044ed
James Croft <jamz_c@hotmail.co.uk>
2015-11-13 23:15:49 +0000

    Added email validation rule for validating text box.

---
+++ Croft.Core/Croft.Core.UWP/Validation/Rules/EmailValidationRule.cs
@@ -0,0 +1,29 @@
+﻿namespace Croft.Core.Validation.Rules
+{
+    using System.Text.RegularExpressions;
+
+    public class EmailValidationRule : ValidationRule
+    {
+        public override string ErrorMessage => "Value is not a valid e-mail.";
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            var val = value.ToString();
+            if (string.IsNullOrWhiteSpace(val))
+            {
+                return true;
+            }
+
+            const string EmailPattern =
+                @"^([0-9a-zA-Z](?>[-.+\w]*[0-9a-zA-Z])*@(?>[0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$";
+
+            var regex = new Regex(EmailPattern, RegexOptions.IgnoreCase);
+            return regex.IsMatch(val);
+        }
+    }
+}
