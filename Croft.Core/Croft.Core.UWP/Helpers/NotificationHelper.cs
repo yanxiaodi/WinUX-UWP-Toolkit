a7a6e0bfd0284d43052a7b8ec06481ad5f6efa15
James Croft <jamz_c@hotmail.co.uk>
2015-11-22 22:20:49 +0000

    Started building up a notification helper for toasts.

---
+++ Croft.Core/Croft.Core.UWP/Helpers/NotificationHelper.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NotificationHelper.cs" company="James Croft">
+//   Copyright (c) 2015 James Croft.
+// </copyright>
+// <summary>
+//   Defines the NotificationHelper type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Croft.Core.Helpers
+{
+    /// <summary>
+    /// The notification helper.
+    /// </summary>
+    public class NotificationHelper
+    {
+
+    }
+}
