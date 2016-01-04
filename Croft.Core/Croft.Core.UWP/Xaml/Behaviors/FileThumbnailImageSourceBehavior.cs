348007bc1e10c9c6b91bc6bf32b2bd9e48f930c7
James Croft <jamz_c@hotmail.co.uk>
2016-01-04 09:22:00 +0000

    Added Behaviors SDK nuget package to the UWP project and pulled across the FileThumbnailImageSourceBehavior from the DragDrop-UWP-Sample app.

---
+++ Croft.Core/Croft.Core.UWP/Xaml/Behaviors/FileThumbnailImageSourceBehavior.cs
@@ -0,0 +1,88 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FileThumbnailImageSourceBehavior.cs" company="James Croft">
+//   Copyright (c) 2015 James Croft.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Croft.Core.Xaml.Behaviors
+{
+    using System;
+
+    using Windows.Storage;
+    using Windows.Storage.FileProperties;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media.Imaging;
+
+    using Microsoft.Xaml.Interactivity;
+
+    /// <summary>
+    /// The file thumbnail image source behavior.
+    /// </summary>
+    public class FileThumbnailImageSourceBehavior : DependencyObject, IBehavior
+    {
+        public static readonly DependencyProperty FileProperty = DependencyProperty.Register(
+            "File",
+            typeof(StorageFile),
+            typeof(FileThumbnailImageSourceBehavior),
+            new PropertyMetadata(null, OnFileChanged));
+
+        private static async void OnFileChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue == null)
+            {
+                return;
+            }
+
+            var behavior = d as FileThumbnailImageSourceBehavior;
+            if (behavior?.Parent == null)
+            {
+                return;
+            }
+
+            var thumb =
+                await behavior.File.GetThumbnailAsync(ThumbnailMode.SingleItem, 32, ThumbnailOptions.ResizeThumbnail);
+
+            if (thumb == null)
+            {
+                return;
+            }
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.SetSource(thumb.CloneStream());
+
+            behavior.Parent.Source = bitmapImage;
+        }
+
+        public StorageFile File
+        {
+            get
+            {
+                return (StorageFile)this.GetValue(FileProperty);
+            }
+            set
+            {
+                this.SetValue(FileProperty, value);
+            }
+        }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (this.AssociatedObject != null)
+            {
+                throw new InvalidOperationException("Cannot assign to the behavior twice.");
+            }
+
+            this.AssociatedObject = associatedObject;
+        }
+
+        public void Detach()
+        {
+            this.AssociatedObject = null;
+        }
+
+        public Image Parent => this.AssociatedObject as Image;
+
+        public DependencyObject AssociatedObject { get; private set; }
+    }
+}
