93f7b9c065f84614a61bb3bb9d2bfe58ad907446
James Croft <jamz_c@hotmail.co.uk>
2016-02-09 12:12:47 +0000

    Updated assembly info and UWP proj to move across to WinUX namespace.

--- Croft.Core/Croft.Core.UWP/Properties/AssemblyInfo.cs
+++ Croft.Core/Croft.Core.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Croft.Core.UWP")]
+[assembly: AssemblyTitle("WinUX.UWP")]
 [assembly: AssemblyDescription("A collection of core XAML controls and extensions to use in your personal universal Windows apps.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("James Croft")]
-[assembly: AssemblyProduct("Croft.Core.UWP")]
-[assembly: AssemblyCopyright("Copyright © 2015 James Croft. All rights reserved.")]
+[assembly: AssemblyProduct("WinUX.UWP")]
+[assembly: AssemblyCopyright("Copyright © 2016 James Croft. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8.*")]
-[assembly: AssemblyFileVersion("1.0.8.0")]
+[assembly: AssemblyVersion("1.2.0.*")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en")]
 
